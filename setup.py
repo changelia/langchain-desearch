abe501190efc38ed42f2692b35db9256559ede2d
changelia <changelia97@gmail.com>
2025-05-05 11:29:35 +0400

    chore: Bump version from 0.1.0 to 1.0.0 in setup.py for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="langchain-desearch",  # Replace with your desired package name
-    version="0.1.0",  # Initial version
+    version="1.0.0",  # Initial version
     author="Desearch",  # Replace with your name
     author_email="your-email@example.com",  # Replace with your email
     description="LangChain integration with Datura API for search and data-fetching tools.",
